f0f7e1508b46e7af010c0999404e5ff698d2ca3d
tanwenfang <1643327713@qq.com>
2018-12-27 11:34:54 +0800

    采用点对点直连的方式，让ribbon的开发调试更方便快捷

---
+++ spring-cloud-in-action/eureka/eureka-consumer-remove-eureka/src/main/java/com/twf/eureka/consumer/remove/eureka/ConsumerRemoveEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.twf.eureka.consumer.remove.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerRemoveEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerRemoveEurekaApplication.class, args);
+	}
+}
