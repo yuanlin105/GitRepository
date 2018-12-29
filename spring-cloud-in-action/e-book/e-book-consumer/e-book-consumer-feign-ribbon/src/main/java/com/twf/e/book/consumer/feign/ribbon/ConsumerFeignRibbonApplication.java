bad5dd8bd8c5465154984c860c8149876870d785
tanwenfang <1643327713@qq.com>
2018-12-29 15:20:03 +0800

    对feign的负载均衡进行优化配置

---
+++ spring-cloud-in-action/e-book/e-book-consumer/e-book-consumer-feign-ribbon/src/main/java/com/twf/e/book/consumer/feign/ribbon/ConsumerFeignRibbonApplication.java
@@ -0,0 +1,16 @@
+package com.twf.e.book.consumer.feign.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerFeignRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerFeignRibbonApplication.class, args);
+	}
+}
