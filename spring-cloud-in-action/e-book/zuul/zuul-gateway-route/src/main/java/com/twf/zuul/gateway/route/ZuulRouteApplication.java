a42d693fc57e6d6e75a39ab6d79efa242084859d
tanwenfang <1643327713@qq.com>
2019-01-17 17:43:49 +0800

    路由器的4种路由规则方法

---
+++ spring-cloud-in-action/e-book/zuul/zuul-gateway-route/src/main/java/com/twf/zuul/gateway/route/ZuulRouteApplication.java
@@ -0,0 +1,14 @@
+package com.twf.zuul.gateway.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulRouteApplication.class, args);
+	}
+}
