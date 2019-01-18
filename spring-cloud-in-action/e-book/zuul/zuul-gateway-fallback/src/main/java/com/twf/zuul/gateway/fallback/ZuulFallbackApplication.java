8a66500da29ea91c69d3b0610f2ef713ae5a4cf2
tanwenfang <1643327713@qq.com>
2019-01-18 16:19:00 +0800

    网关容错：网关如何实现服务降级？

---
+++ spring-cloud-in-action/e-book/zuul/zuul-gateway-fallback/src/main/java/com/twf/zuul/gateway/fallback/ZuulFallbackApplication.java
@@ -0,0 +1,14 @@
+package com.twf.zuul.gateway.fallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulFallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulFallbackApplication.class, args);
+	}
+}
