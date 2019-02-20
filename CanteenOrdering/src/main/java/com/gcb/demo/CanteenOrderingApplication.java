9db07f8bd5b0594cdfda7fd838cae383294ec9b0
tanwenfang <1643327713@qq.com>
2019-02-20 10:43:22 +0800

    提交食堂点餐系统的初始代码

---
+++ CanteenOrdering/src/main/java/com/gcb/demo/CanteenOrderingApplication.java
@@ -0,0 +1,13 @@
+package com.gcb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanteenOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanteenOrderingApplication.class, args);
+	}
+
+}
---
+++ CanteenOrdering/src/test/java/com/gcb/demo/CanteenOrderingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gcb.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CanteenOrderingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
