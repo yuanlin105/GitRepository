281dd3c193d70a7152202ec8765718a7afcc4b1c
tanwenfang <1643327713@qq.com>
2019-03-23 19:52:13 +0800

    2

--- spring-cloud-in-action/e-book/sleuth/sleuth-zipkin-mysql-server/src/main/java/com/twf/sleuth/zipkin/mysql/server/ZipkinMysqlApplication.java
+++ spring-cloud-in-action/e-book/sleuth/sleuth-zipkin-mysql-server/src/main/java/com/twf/sleuth/zipkin/mysql/server/ZipkinMysqlApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 @EnableZipkinStreamServer // 开启Zipkin通过消息中间件方式收集链路跟踪信息功能
 public class ZipkinMysqlApplication {
 
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		SpringApplication.run(ZipkinMysqlApplication.class, args);
 	}
 }
