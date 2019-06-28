9b77274fddecd21f9804eb574a66e69f789fc5ab
yuanlin <jatly22@126.com>
2019-06-28 14:25:06 +0800

    完善负载均衡调用 配置文件

--- spring-cloud-in-action/eureka/eureka-consumer-Lb/src/main/java/com/twf/eureka/consumer/Lb/ConsumerLbApplication.java
+++ spring-cloud-in-action/eureka/eureka-consumer-Lb/src/main/java/com/twf/eureka/consumer/Lb/ConsumerLbApplication.java
@@ -3,15 +3,23 @@ package com.twf.eureka.consumer.Lb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class ConsumerLbApplication {
-
-//	@Bean
-//	public IRule ribbonRule() {  // 配置负载均衡策略
-//		return new RandomRule(); // 这里配置的是随机策略
-//	}
+	
+	/**与appliacation.properties 中配置 如下指令等效
+	 * eureka-provider.ribbon.NFLoadBalancerRuleClassName=com.netflix.loadbalancer.RandomRule
+	 * @return
+	 */
+	@Bean
+	public IRule ribbonRule() {  // 配置负载均衡策略
+		return new RandomRule(); // 这里配置的是随机策略
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerLbApplication.class, args);
